21dcc455561744c577728d8071a87d80b61573fc
Daviiduhh <daviiduhhs@gmail.com>
2024-08-05 12:21:30 -0600

    Se agrega nueva version de font awesome

---
+++ src/fontAwesomeIcons.js
@@ -0,0 +1,49 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import {
+  faGlobeAmericas,
+  faBars,
+  faX,
+  faEnvelope,
+  faTerminal,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faGitlab,
+  faLinkedin,
+  faTwitter,
+  faHtml5,
+  faCss3,
+  faJs,
+  faGitAlt,
+  faSass,
+  faGulp,
+  faNpm,
+  faVuejs,
+  faBootstrap,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faGithub,
+  faGitlab,
+  faLinkedin,
+  faTwitter,
+  faGlobeAmericas,
+  faBars,
+  faX,
+  faEnvelope,
+  faHtml5,
+  faCss3,
+  faJs,
+  faGitAlt,
+  faSass,
+  faGulp,
+  faNpm,
+  faVuejs,
+  faTerminal,
+  faBootstrap
+);
+
+export default FontAwesomeIcon;
--- src/main.js
+++ src/main.js
@@ -1,56 +1,20 @@
-import { createApp } from "vue";
+import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
 import router from "./router";
 
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faGlobeAmericas,
-  faBars,
-  faX,
-  faEnvelope,
-  faTerminal,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithub,
-  faGitlab,
-  faLinkedin,
-  faTwitter,
-  faHtml5,
-  faCss3,
-  faJs,
-  faGitAlt,
-  faSass,
-  faGulp,
-  faNpm,
-  faVuejs,
-  faBootstrap,
-} from "@fortawesome/free-brands-svg-icons";
+import FontAwesomeIcon from "./fontAwesomeIcons";
 
 import App from "./App.vue";
 
 const pinia = createPinia(),
   app = createApp(App);
 
-library.add(
-  faGithub,
-  faGitlab,
-  faLinkedin,
-  faTwitter,
-  faGlobeAmericas,
-  faBars,
-  faX,
-  faEnvelope,
-  faHtml5,
-  faCss3,
-  faJs,
-  faGitAlt,
-  faSass,
-  faGulp,
-  faNpm,
-  faVuejs,
-  faTerminal,
-  faBootstrap
-);
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
 
-app.component("fa", FontAwesomeIcon).use(pinia).use(router).mount("#app");
+app
+  .component("fa", FontAwesomeIcon)
+  .use(pinia)
+  .use(router)
+  .mount("#app");
